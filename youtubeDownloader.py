e2100845d8743141c9a71b4b00c7ab2b89d17501
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-11-17 03:38:39 +0330

    Developing this project

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,7 +1,12 @@
 from pytube import YouTube
+from colorama import init,Fore 
 
-url = input(" please define URL : ")
+print(Fore.YELLOW+'\n---------- Welcome to YouTube Downloader ----------\n')
+
+url = input(Fore.BLUE+' [ * ] '+Fore.WHITE+'Enter URL : ')
+print(Fore.GREEN+'\n i trying concten to '+url)
 video = YouTube(url)
 stream = video.streams.get_highest_resolution()
 
-stream.download(output_path= '/')
+locate = input(Fore.BLUE+' [ ? ] '+Fore.WHITE+'location for Download video : ')
+stream.download(output_path = locate)
